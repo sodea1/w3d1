cdc5faa831d34bb7efbe7523eee8b58dc7aa995f
Jackson <jacksondooley24@gmail.com>
2022-02-14 15:19:42 -0500

    jackson first commit

--- enumerables.rb
+++ enumerables.rb
@@ -1,1 +1,22 @@
-def 
+class Array
+        
+    def my_each(&prc)
+        self.length - 1.times { |el| prc.call(el) }  
+        self  
+    end
+
+end
+
+return_value = [1, 2, 3].my_each do |num|
+    puts num
+   end.my_each do |num|
+    puts num
+   end
+   # => 1
+       2
+       3
+       1
+       2
+       3
+   
+   p return_value  # => [1, 2, 3]
