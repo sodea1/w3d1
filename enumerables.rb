5116b9e45f21eed992dfcd17d5f934f7c3c5687e
Jackson <jacksondooley24@gmail.com>
2022-02-14 15:55:59 -0500

    jackson second commit

--- enumerables.rb
+++ enumerables.rb
@@ -1,22 +1,89 @@
+require "byebug"
+
 class Array
         
     def my_each(&prc)
-        self.length - 1.times { |el| prc.call(el) }  
-        self  
+        i = 0
+        while i < self.length
+            prc.call(self[i])
+            i += 1
+        end
+        self
     end
 
+    def my_select(&prc)
+        arr = []
+        self.my_each do |ele|
+            arr << ele if prc.call(ele)
+        end
+        arr
+    end
+
+    def my_reject(&prc)
+        arr = []
+        self.my_each do |ele|
+            arr << ele if ! prc.call(ele)
+        end
+        arr
+    end
+
+    def my_any?(&prc)
+        arr = []
+        self.my_each do |ele|
+            return true if prc.call(ele)
+        end
+        false
+    end
+
+    def my_all?(&prc)
+        arr = []
+        self.my_each do |ele|
+            return false if ! prc.call(ele)
+        end
+        true
+    end
+
+    def my_flatten
+        debugger
+        return [self] if ! self.is_a? Array
+        flattened = []
+        self.each do |el|
+            flattened << el if el
+            flattened += el.my_flatten
+        end
+        flattened
+    end
+
+
+    
 end
 
-return_value = [1, 2, 3].my_each do |num|
-    puts num
-   end.my_each do |num|
-    puts num
-   end
-   # => 1
-       2
-       3
-       1
-       2
-       3
+# return_value = [1, 2, 3].my_each do |num|
+#     puts num
+#    end.my_each do |num|
+#     puts num
+#    end
+#    # => 1
+#        2
+#        3
+#        1
+#        2
+#        3
    
-   p return_value  # => [1, 2, 3]
+#    p return_value  # => [1, 2, 3]
+
+#    a = [1, 2, 3]
+#    p a.my_select { |num| num > 1 } # => [2, 3]
+#    p a.my_select { |num| num == 4 } # => []
+
+# a = [1, 2, 3]
+# p a.my_reject { |num| num > 1 } # => [1]
+# p a.my_reject { |num| num == 4 } # => [1, 2, 3]
+
+# a = [1, 2, 3]
+# p a.my_any? { |num| num > 1 } # => true
+# p a.my_any? { |num| num == 4 } # => false
+# p a.my_all? { |num| num > 1 } # => false
+# p a.my_all? { |num| num < 4 } # => true
+
+p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
