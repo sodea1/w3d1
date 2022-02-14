b491995b03ff14a4d8092471dc4a4826dc26d6c9
seanodea <seanodea11@gmail.com>
2022-02-14 16:28:38 -0500

    sean commit

--- enumerables.rb
+++ enumerables.rb
@@ -44,7 +44,7 @@ class Array
     end
 
     def my_flatten
-        debugger
+        # debugger
         return [self] if ! self.is_a? Array
         flattened = []
         self.each do |el|
@@ -54,9 +54,41 @@ class Array
         flattened
     end
 
+    def my_zip(*args)
+        arr = Array.new(self.length) { Array.new(args.length + 1, nil) }
 
+        self.each.with_index do |ele, i|
+            arr[i][0] = ele
+        end
+
+        # args = [ 4, 5, 6 ], [ 7, 8, 9 ]
+        # arg = [ 7, 8, 9 ]
+        # i = 2
+
+        # [ [1, , ] [2, , ] [3, , ] ]
+
+        i = 1
+
+        args.each do |arg| 
+            arg.each.with_index do |ele, el_i|
+                arr[el_i][i] = ele
+            end
+            i += 1
+        end
+        arr
+    end
     
 end
+
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
 
 # return_value = [1, 2, 3].my_each do |num|
 #     puts num
@@ -86,4 +118,4 @@ end
 # p a.my_all? { |num| num > 1 } # => false
 # p a.my_all? { |num| num < 4 } # => true
 
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+# p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
