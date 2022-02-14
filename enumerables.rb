f1f3c28d6704bef176d1bad7c17276e807823d2c
Jackson <jacksondooley24@gmail.com>
2022-02-14 16:47:44 -0500

    jackson commit

--- enumerables.rb
+++ enumerables.rb
@@ -77,18 +77,62 @@ class Array
         end
         arr
     end
+
+    def my_rotate(n=1)
+        arr = self.dup
+        if n > 0
+            n.times do
+                first = arr.shift
+                arr << first
+            end
+        elsif n < 0
+            n.abs.times do 
+                last = arr.pop
+                arr.unshift(last)
+            end
+        end
+        arr
+    end
+
+    def my_join(char="")
+        str = ""
+        self.each { |c| str += c + char}
+        return str
+    end
+
+    def my_reverse
+        reversed = []
+        self.length.times do 
+            reversed << self.pop
+        end
+        reversed
+    end
     
 end
 
-a = [ 4, 5, 6 ]
-b = [ 7, 8, 9 ]
-p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
-p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
-p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+p [ "a", "b", "c" ].my_reverse   #=> ["c", "b", "a"]
+p [ 1 ].my_reverse               #=> [1]
 
-c = [10, 11, 12]
-d = [13, 14, 15]
-p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
+# a = [ "a", "b", "c", "d" ]
+# p a.my_join         # => "abcd"
+# p a.my_join("$")    # => "a$b$c$d"
+
+
+# a = [ "a", "b", "c", "d" ]
+# p a.my_rotate         #=> ["b", "c", "d", "a"]
+# p a.my_rotate(2)      #=> ["c", "d", "a", "b"]
+# p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
+# p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
+
+# a = [ 4, 5, 6 ]
+# b = [ 7, 8, 9 ]
+# p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+# p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+# p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+# c = [10, 11, 12]
+# d = [13, 14, 15]
+# p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
 
 # return_value = [1, 2, 3].my_each do |num|
 #     puts num
